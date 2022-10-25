f75d1aeb2c3c4ace114b5669711b67cbbf5741af
LautaroAntriolo <lautaroantriololuis@gmail.com>
2022-10-25 17:22:12 -0300

    Prueba de entorno

--- Datos/preuba.py
+++ Datos/preuba.py
@@ -7,11 +7,3 @@ for a in num:
 
 # print("Tomate")
 
-import pyqrcode
-import png
-def crearQR(Link, nombre):
-    link = Link
-    qr_code = pyqrcode.create(link, version=5)
-    return qr_code.png(nombre, scale=5)
-
-crearQR('https://youtu.be/0wuahFO6apg','Prueba3')
--- QR_python/CodigoQR.py
+++ QR_python/CodigoQR.py
@@ -1,1 +1,9 @@
 
+import pyqrcode
+import png
+def crearQR(Link, nombre):
+    link = Link
+    qr_code = pyqrcode.create(link, version=5)
+    return qr_code.png(nombre, scale=5)
+
+crearQR('https://youtu.be/0wuahFO6apg','Prueba3')
