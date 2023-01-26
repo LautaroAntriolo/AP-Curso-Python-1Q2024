c9fe4417bd730c492f152bb19a137284b2c864fa
LautaroAntriolo <lautaroantriololuis@gmail.com>
2023-01-26 09:51:37 -0300

    "cambios generales"

--- Clase_8_Google/lectura.py
+++ Clase_8_Google/lectura.py
@@ -27,5 +27,30 @@ valores = resultados['values']
 ########################################################################################################################
 
 
+#%%
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 
-# %%
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+KEY = 'key.json'
+
+# Escribe aquí el ID de tu documento:
+SPREADSHEET_ID = 'el ID que creaste'
+
+
+creds = None
+# Funión a la que le pasamos como parámetros los valores que guardamos en las variables anteriormente.
+creds = service_account.Credentials.from_service_account_file(KEY, scopes=SCOPES)
+
+service = build('sheets', 'v4', credentials=creds)
+sheet = service.spreadsheets()
+
+
+#%%
+# Llamamos a la API
+resultados = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range='Respuestas de Formulario 2!F2:F34',majorDimension='COLUMNS').execute()
+# print(resultados)
+# Extraemos values del resultado
+valores = resultados['values']
+# print(valores[0])
--- Clase_9_Scrapping/beauty/beauty.py
+++ Clase_9_Scrapping/beauty/beauty.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 pagina_web = 'https://lautaroantriolo.netlify.app/'
 solicitud = requests.get(pagina_web)
 # print(solicitud)
+
 #%%
 # .text te devuelve todo el contenido de la página inluidas las APIS, urls etc
 contenido = solicitud.text
@@ -28,14 +29,28 @@ print(articulo.attrs)
 # Prettify => te devuelve el código HTML 
 # find => Te devuelve la primer etiqueta que le pasemos como parámetro
 # find_all => Te devuelve todas las etiquetas que le pasemos como parámetro
+#   Find es mucho mas óptima que find_all, pero como no sabemos bien cual es el contenido, usamod find_all hasta saber que existe un solo elemento.
 # acceder a los hijos => Abuelo.padre.hijo . Solo devuelve el primer hijo que corresponda con esta estructura/etiqueta
+# Podemos acceder a el contenido de la etiqueta de ls siguiente forma:
+#   titulo = entrada.find('h2', {'class' : 'titulo'}).getText() # Encuentro los h2 que tengan la clase titulo y con getText() extraigo su contenido. 
+#   Si no uso el método getText() entonces mostraría la etiqueta e incluso el contenido, pero solo quiero el contenido.
+# class_ => Para encontrar los elementos por las clases de CSS . soup.find_all(class_ ="Clase1 Clase2 ClsaeN")
+# Id => Para encontrar los elementos por el id de CSS soup.find_all(id='footer')
+
+
 
 
 #%%
 # Encontrar los datos de interés utilizando las etiquetas HTML
-datos = soup.find_all('div', class_='datos')
+# datos = soup.find_all('div', class_='datos')
+for link in soup.find_all('a'):
+  if 'http' in link:
+    print(link.get('href'))
+  else:
+    print(link.get('href'))
 
 # Imprimir los datos
+#%%
 for dato in datos:
   print(dato.text)
 # %%
