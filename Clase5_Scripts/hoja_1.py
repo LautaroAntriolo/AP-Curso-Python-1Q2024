e5ed6c98b844964bb5f5006fa840af19c346b3d2
LautaroAntriolo <lautaroantriololuis@gmail.com>
2023-03-01 18:01:59 -0300

    clase 5 códigos

--- Clase5_Scripts/hoja_1.py
+++ Clase5_Scripts/hoja_1.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# Dos funciones que podemos usar tranquilamente desde la consola y que 
+# son fáciles de implementar
 def factorial(x):
     fac = np.math.factorial(x)
     return fac
@@ -9,4 +11,26 @@ def cantEnteros(n,k):
     print(resultado)
     return resultado
 
-cantEnteros(6,3)
+def cantLetras(palabra):
+    cant = len(palabra)
+    return cant
+
+def cantLetras2(palabra):
+    cant = 0
+    letras = 'abcdefghijklmnñopqrstuvwxyz'
+    # probar que hay que tener en cuenta los acentos.
+    # letras = 'aábcdeéfghiíjklmnñoópqrstuúvwxyz'
+
+    for i in (palabra):
+        if i in letras:
+            cant +=1
+    return cant
+
+if __name__=='__main__':
+    ## Acá dentro ejecutamos las funciones que queremos:
+    cantidad = cantLetras2("El agua no se mexcla con el aceite")
+    print(cantidad)
+    numero = int(input("ingresá un numero: \n"))
+    print(factorial(numero))
+
+
